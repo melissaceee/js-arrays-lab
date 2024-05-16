a3924eccd24da8ec9d1395f722cc23485b7229c2
melissaceee <cheng.melissa@outlook.com>
2024-05-15 22:29:35 -0400

    exercise 5

--- app.js
+++ app.js
@@ -54,7 +54,22 @@ Exercise 4: Access an array element
 Complete Exercise 4 in the space below:
 */
 
-const foods = ['sushi', 'pizza', 'cake']
-const favFood = foods [1];
+// const foods = ['pizza', 'cheeseburger', 'sushi']
+// const favFood = foods [0];
 
-console.log('Exercise 4 result:', favFood);
+// console.log('Exercise 4 result:', favFood);
+
+
+/*
+Exercise 5: Insert an element between two others
+
+1) Insert the string 'tofu' between 'pizza' and 'cheeseburger' in the array.
+
+Complete Exercise 5 in the space below:
+*/
+
+const foods = ['pizza', 'cheeseburger', 'sushi']
+foods.splice(0, 0, 'tofu')
+
+console.log('Exercise 5 result:', foods);
+
